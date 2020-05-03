47b276886e29e4bc18ea57db07c9fb381cea688f
Teógenes Teles <teocteles@gmail.com>
2020-05-03 20:59:55 -0300

    Ajustando a porta

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 // app.get('/', (req, res) => {
 //   res.sendFile(__dirname + '/index.html');
