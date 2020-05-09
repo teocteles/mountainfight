8cfd4e816b3cd5c32aba5a1bd68f368711b9deb0
Teógenes Teles <teocteles@gmail.com>
2020-05-08 21:29:19 -0300

    Atualizacoes e criacao do do attack

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 // app.get('/chat', (req, res) => {
 //   res.sendFile(__dirname + '/index.html');
